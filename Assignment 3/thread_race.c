3ec34203d988e3d9e62ff7d3833d0bba0f8ff506
jkcadee <jkwok55@my.bcit.ca>
2021-10-04 15:21:44 -0700

    mutex implemented

--- Assignment 3/thread_race.c
+++ Assignment 3/thread_race.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <pthread.h>
-
-#define MAX_THREADS 50
 
 struct ThreadFuncArgs {
     int numOfLoops;
@@ -10,6 +9,7 @@ struct ThreadFuncArgs {
 };
 
 int globalVal = 0;
+pthread_mutex_t lock;
 
 void* threadLooping(void* vargp) {
     struct ThreadFuncArgs *args = (struct ThreadFuncArgs *)vargp;
@@ -22,40 +22,85 @@ void* threadLooping(void* vargp) {
     return NULL;
 }
 
+void* mutexThreadLooping(void* vargp) {
+    pthread_mutex_lock(&lock);
+
+    struct ThreadFuncArgs *args = (struct ThreadFuncArgs *)vargp;
+
+    for (int i = 0; i < args->numOfLoops; i++) {
+        printf("%s: %d\n",args->threadName, globalVal);
+        globalVal++;
+        for (int i = 0; i < args->numOfLoops; i++);
+    }
+
+    pthread_mutex_unlock(&lock);
+    return NULL;
+}
+
 int main(int argc, char *argv[]) {
     int threadAmount = 0;
 
-    for (int i = 0; i < MAX_THREADS; i++)
-    {
-        if (i > 1 && argv[i] != NULL) {
-            threadAmount++;
-        }
+    if (strcmp(argv[1], "-mutex") == 0) {
+        for (int i = 0; i < argc; i++)
+        {
+            if (i > 2 && argv[i] != NULL) {
+                threadAmount++;
+            }
 
-        if (argv[i] == NULL) {
-            break;
+            if (argv[i] == NULL) {
+                break;
+            }
+
+        }
+    } else {
+        for (int i = 0; i < argc; i++)
+        {
+            if (i > 1 && argv[i] != NULL) {
+                threadAmount++;
+            }
+
+            if (argv[i] == NULL) {
+                break;
+            }
         }
     }
 
     pthread_t threads[threadAmount];
 
     struct ThreadFuncArgs args[threadAmount];
-    
-    for (int i = 0; i < threadAmount; i++)
-    {
-        args[i].numOfLoops = atoi(argv[1]);
-        args[i].threadName = argv[i + 2];
-    }
-    
 
-    for (int i = 0; i < threadAmount; i++)
-    {
-        pthread_create(&threads[i], NULL, threadLooping, (void*)&args[i]);
+    if (strcmp(argv[1], "-mutex") == 0) {
+        for (int i = 0; i < threadAmount; i++)
+        {
+            args[i].numOfLoops = atoi(argv[2]);
+            args[i].threadName = argv[i + 3];
+        }
+
+        for (int i = 0; i < threadAmount; i++)
+        {
+            pthread_mutex_init(&lock, NULL);
+            pthread_create(&threads[i], NULL, mutexThreadLooping, (void*)&args[i]);
+        }
+    } else {
+        for (int i = 0; i < threadAmount; i++)
+        {
+            args[i].numOfLoops = atoi(argv[1]);
+            args[i].threadName = argv[i + 2];
+        }
+
+        for (int i = 0; i < threadAmount; i++)
+        {
+            pthread_create(&threads[i], NULL, threadLooping, (void*)&args[i]);
+        }
     }
 
     for (int i = 0; i < threadAmount; i++)
     {
         pthread_join(threads[i], NULL);
+        pthread_mutex_destroy(&lock);
     }
-        pthread_exit(NULL);    
+        printf("Final: %d\n", globalVal); 
+        pthread_exit(NULL);   
     return 0;
+
 }
