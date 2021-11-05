3ea54a5cd42917a12622df0106780003c33e874c
jkcadee <jkwok55@my.bcit.ca>
2021-11-04 22:47:37 -0700

    commented code

--- Assignment 4/primeMain.c
+++ Assignment 4/primeMain.c
@@ -8,15 +8,17 @@ This is a program that prints the Nth prime number
 #include <string.h>
 #include "prime.h"
 
+// provides usage for the function
 static void usage() {
         printf("Usage: primeMain [-list] a1\n");
         exit(1);
 }
 
 int main(int argc, char** argv) {
+    // variable initialization 
     int i;
-    bool list = false;
-    int numToFind;
+    bool list = false; // will determine if all the prime vals are listed or not
+    int numToFind; 
     int n_th_prime;
     int *primeList;
 
@@ -26,9 +28,10 @@ int main(int argc, char** argv) {
             break;
         }
 
-        if (!strcmp(argv[i], "-list")) {
+        if (!strcmp(argv[i], "-list")) { // flag list as true, all primes will be printed
             list = true;
         } else {
+            // print usage if there are any issues with the arguments
             usage();
         }
     }
@@ -38,22 +41,30 @@ int main(int argc, char** argv) {
         usage();
     }
 
+    // get the number to find from the arguments
     numToFind = atoi(argv[i++]);
 
+    // if list is true
     if (list) {
+        // allocate a list of primes
         primeList = (int*)malloc(numToFind*sizeof(int));
+        // find the n_th prime number
         n_th_prime = prime(numToFind, primeList);
 
+        // print each value in the prime list
         for (int i = 0; i < numToFind; i++) {
             printf("%d ", primeList[i]);
         }
 
         printf("\n");
+        // free the memory
         free(primeList);
     } else {
+        // find the n_th prime number
         n_th_prime = prime(numToFind, NULL);
     }
 
+    // print the n_th prime
     printf("%d\n", n_th_prime);
 
 }
--- Assignment 4/skel.c
+++ Assignment 4/skel.c
@@ -7,14 +7,16 @@
 #include <unistd.h>
 #include <string.h>
 
+// provides usage for the function
 static void usage() {
     printf("Usage: skel [-o1 v] [-o2] a1 a2\n");
     exit(1);
 }
 
 int main(int argc, char **argv) {
-    int i;
-    int o1;
+    // variable initialization 
+    int i; // index
+    int o1; 
     bool o2;
     int a1;
     float a2;
@@ -23,19 +25,23 @@ int main(int argc, char **argv) {
     o1 = 10;
     o2 = false;
 
+    // for each argument passed through the command line
     for (i = 1; i < argc; i++) {
-        if (argv[i][0] != '-') {
+        if (argv[i][0] != '-') { // no optional arg given
             break;
         }
-
+        // if the current argument is '-o1'
         if (!strcmp(argv[i], "-o1")) {
+            // if there is an error with num of arguments, print out usage
             if (++i == argc) {
                 usage();
             }
+            // get the argument
             o1 = atoi(argv[i]);
         } else if (!strcmp(argv[i], "-o2")) {
-            o2 = true;
+            o2 = true; // get true if an "-o2" is provided
         } else {
+            // usage printed on incorrect arguments
             usage();
         }
     }
@@ -45,8 +51,10 @@ int main(int argc, char **argv) {
         usage();
     }
 
+    // get the integer and float arguments
     a1 = atoi(argv[i++]);
     a2 = atof(argv[i++]);
 
+    // print all the provided arguments
     printf("o1=%d, o2=%s, a1=%d, a2=%f\n", o1, o2 ? "T" : "F", a1, a2);
 }
