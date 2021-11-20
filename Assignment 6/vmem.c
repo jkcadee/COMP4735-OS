33741a85ffa876ae207b7f9f9ec618a68bf8b48e
Janelle Kwok <54956049+jkcadee@users.noreply.github.com>
2021-11-20 15:53:57 -0800

    entry won't update properly (value is overwritten), some work is done

--- Assignment 6/vmem.c
+++ Assignment 6/vmem.c
@@ -8,9 +8,8 @@
 
 typedef struct {
     unsigned char num;
-    int pageNumber;
     int mappedVal;
-    char** address;
+    char* address;
 } PageTableEntry;
 
 typedef struct {
@@ -21,122 +20,124 @@ typedef struct {
 
 static PageTable table;
 
-void convertToBinary(char *numToConvert) {
-    PageTableEntry *entry;
-    entry = &table.entries[table.count++];
-    int index;
+void convertToBinary(char *numToConvert, PageTableEntry *entry) {
+    int index = 0;
     int addressIndex = 0;
     char address[SIZE_OF_ADDRESS];
     while(numToConvert[index]) {
         switch (numToConvert[index]) {
-        case '0':
-                address[addressIndex] = 0;
-                address[addressIndex + 1] = 0;
-                address[addressIndex + 2] = 0;
-                address[addressIndex + 3] = 0;
+            case '0':
+                address[addressIndex] = '0';
+                address[addressIndex + 1] = '0';
+                address[addressIndex + 2] = '0';
+                address[addressIndex + 3] = '0';
                 break;
             case '1':
-                address[addressIndex] = 0;
-                address[addressIndex + 1] = 0;
-                address[addressIndex + 2] = 0;
-                address[addressIndex + 3] = 1;
+                address[addressIndex] = '0';
+                address[addressIndex + 1] = '0';
+                address[addressIndex + 2] = '0';
+                address[addressIndex + 3] = '1';
                 break;
             case '2':
-                address[addressIndex] = 0;
-                address[addressIndex + 1] = 0;
-                address[addressIndex + 2] = 1;
-                address[addressIndex + 3] = 0;
+                address[addressIndex] = '0';
+                address[addressIndex + 1] = '0';
+                address[addressIndex + 2] = '1';
+                address[addressIndex + 3] = '0';
                 break;
             case '3':
-                address[addressIndex] = 0;
-                address[addressIndex + 1] = 0;
-                address[addressIndex + 2] = 1;
-                address[addressIndex + 3] = 1;
+                address[addressIndex] = '0';
+                address[addressIndex + 1] = '0';
+                address[addressIndex + 2] = '1';
+                address[addressIndex + 3] = '1';
                 break;
             case '4':
-                address[addressIndex] = 0;
-                address[addressIndex + 1] = 1;
-                address[addressIndex + 2] = 0;
-                address[addressIndex + 3] = 0;
+                address[addressIndex] = '0';
+                address[addressIndex + 1] = '1';
+                address[addressIndex + 2] = '0';
+                address[addressIndex + 3] = '0';
                 break;
             case '5':
-                address[addressIndex] = 0;
-                address[addressIndex + 1] = 1;
-                address[addressIndex + 2] = 0;
-                address[addressIndex + 3] = 1;
+                address[addressIndex] = '0';
+                address[addressIndex + 1] = '1';
+                address[addressIndex + 2] = '0';
+                address[addressIndex + 3] = '1';
                 break;
             case '6':
-                address[addressIndex] = 0;
-                address[addressIndex + 1] = 1;
-                address[addressIndex + 2] = 1;
-                address[addressIndex + 3] = 0;
+                address[addressIndex] = '0';
+                address[addressIndex + 1] = '1';
+                address[addressIndex + 2] = '1';
+                address[addressIndex + 3] = '0';
                 break;
             case '7':
-                address[addressIndex] = 0;
-                address[addressIndex + 1] = 1;
-                address[addressIndex + 2] = 1;
-                address[addressIndex + 3] = 1;
+                address[addressIndex] = '0';
+                address[addressIndex + 1] = '1';
+                address[addressIndex + 2] = '1';
+                address[addressIndex + 3] = '1';
                 break;
             case '8':
-                address[addressIndex] = 1;
-                address[addressIndex + 1] = 0;
-                address[addressIndex + 2] = 0;
-                address[addressIndex + 3] = 0;
+                address[addressIndex] = '1';
+                address[addressIndex + 1] = '0';
+                address[addressIndex + 2] = '0';
+                address[addressIndex + 3] = '0';
                 break;
             case '9':
-                address[addressIndex] = 1;
-                address[addressIndex + 1] = 0;
-                address[addressIndex + 2] = 0;
-                address[addressIndex + 3] = 1;
+                address[addressIndex] = '1';
+                address[addressIndex + 1] = '0';
+                address[addressIndex + 2] = '0';
+                address[addressIndex + 3] = '1';
                 break;
             case 'A':
             case 'a':
-                address[addressIndex] = 1;
-                address[addressIndex + 1] = 0;
-                address[addressIndex + 2] = 1;
-                address[addressIndex + 3] = 0;
+                address[addressIndex] = '1';
+                address[addressIndex + 1] = '0';
+                address[addressIndex + 2] = '1';
+                address[addressIndex + 3] = '0';
                 break;
             case 'B':
             case 'b':
-                address[addressIndex] = 1;
-                address[addressIndex + 1] = 0;
-                address[addressIndex + 2] = 1;
-                address[addressIndex + 3] = 1;
+                address[addressIndex] = '1';
+                address[addressIndex + 1] = '0';
+                address[addressIndex + 2] = '1';
+                address[addressIndex + 3] = '1';
                 break;
             case 'C':
             case 'c':
-                address[addressIndex] = 1;
-                address[addressIndex + 1] = 1;
-                address[addressIndex + 2] = 0;
-                address[addressIndex + 3] = 0;
+                address[addressIndex] = '1';
+                address[addressIndex + 1] = '1';
+                address[addressIndex + 2] = '0';
+                address[addressIndex + 3] = '0';
                 break;
             case 'D':
             case 'd':
-                address[addressIndex] = 1;
-                address[addressIndex + 1] = 1;
-                address[addressIndex + 2] = 0;
-                address[addressIndex + 3] = 1;
+                address[addressIndex] = '1';
+                address[addressIndex + 1] = '1';
+                address[addressIndex + 2] = '0';
+                address[addressIndex + 3] = '1';
                 break;
             case 'E':
             case 'e':
-                address[addressIndex] = 1;
-                address[addressIndex + 1] = 1;
-                address[addressIndex + 2] = 1;
-                address[addressIndex + 3] = 0;
+                address[addressIndex] = '1';
+                address[addressIndex + 1] = '1';
+                address[addressIndex + 2] = '1';
+                address[addressIndex + 3] = '0';
                 break;
             case 'F':
             case 'f':
-                address[addressIndex] = 1;
-                address[addressIndex + 1] = 1;
-                address[addressIndex + 2] = 1;
-                address[addressIndex + 3] = 1;
+                address[addressIndex] = '1';
+                address[addressIndex + 1] = '1';
+                address[addressIndex + 2] = '1';
+                address[addressIndex + 3] = '1';
                 break;
         }
+        index++;
+        addressIndex = addressIndex + 4;
     }
+    entry->address = address;
+    printf("address: %s\n", table.entries[0].address);
 }
 
 void addMapping(char *mappedVal) {
-    table.arrayOfPageMap[mappedVal[0]] = mappedVal[strlen(mappedVal) - 1];
+    table.arrayOfPageMap[(int) mappedVal[0]] = mappedVal[strlen(mappedVal) - 1];
 }
 
 bool readPageMapFile(char *fn) {
@@ -163,7 +164,6 @@ bool readPageMapFile(char *fn) {
 
 bool readMemRefFile(char *fn) {
     FILE *fp;
-
     if (!(fp = fopen(fn, "r"))) { // if there is an error with opening the file
         // print an error message and return false
         printf("Cannot open '%s' for reading\n", fn);
@@ -172,13 +172,19 @@ bool readMemRefFile(char *fn) {
     
     #define MAX_LINE_LEN 80 // max line length for file
     char line[MAX_LINE_LEN];
- // entry in the page table
 
     while ((fscanf(fp, "%s", line)) != EOF) {
-        convertToBinary(line);
+        PageTableEntry* entry = &table.entries[table.count++];
+        convertToBinary(line, entry);
+        //printf("entry: address %s\n", table.entries[table.count].address);
+        // for (int i = 0; i < table.count; i++) {
+        //     printf("entry: address %s\n", table.entries[i].address);
+        // }
     }
-    
 
+    // for (int i = 0; i < table.count; i++) {
+    //     printf("entry: address %s\n", table.entries[i].address);
+    // }
 
     // close the file and return true
     fclose(fp);
@@ -186,17 +192,23 @@ bool readMemRefFile(char *fn) {
 }
 
 int main(int argc, char **argv) {
-    FILE *fp;
     table.count = 0;
-    
+
     for (int i = 0; i < argc; i++) {
-        if (i == 2) {
+        if (i == 1) {
             readMemRefFile(argv[i]);
-        } else if (i == 3) {
+        } else if (i == 2) {
             readPageMapFile(argv[i]);
         }
     }
     
-
+    // for (int i = 0; i < table.count; i++) {
+    //     printf("entry: address %s\n", table.entries[i].address);
+    // }
+    
+    for (int i = 0; i < table.count; i++) {
+        
+    }
+    
 
 }
